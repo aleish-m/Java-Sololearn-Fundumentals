fc4daa34fb181983fba89c6392d76e7b092a934b
Aleisha Mork <aleisha.mork@gmail.com>
2023-05-11 15:59:39 -0600

    Methods with Paramaters Challenge

---
+++ methods/MethodParameters.java
@@ -0,0 +1,30 @@
+package methods;
+import java.util.Scanner;
+
+/*
+  Code Challenge - Feet to Inches Converter
+
+    You need to make a method that converts a foot value to inches.
+      1 foot has 12 inches. 
+  
+    Define a convert() method, that takes the foot value as its argument and outputs the inches value.
+    The result must be a double.
+
+    The given code takes the foot value as input and passes it to the convert method. Define the convert method, so that the given code works.
+*/
+
+public class MethodParameters {
+  // your code goes here
+
+  public static void ain(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double num = sc.nextDouble();
+
+    convert(num);
+  }
+
+  static void convert(double feet) {
+    double res = feet * 12;
+    System.out.println(res);
+  }
+}
